cda796924a50acc81b67e596f4612f9156e7c2c0
Leandro Atallah <leandroatallah@gmail.com>
2025-10-31 11:40:33 +0000

    implement track selection scene

--- internal/game/scenes/main.go
+++ internal/game/scenes/main.go
@@ -9,13 +9,15 @@ const (
 	SceneIntro navigation.SceneType = iota
 	SceneMenu
 	ScenePlay
+	SceneTrackSelection
 )
 
 func InitSceneMap(context *core.AppContext) map[navigation.SceneType]navigation.Scene {
 	SceneMap := map[navigation.SceneType]navigation.Scene{
-		SceneIntro: NewIntroScene(context),
-		SceneMenu:  NewMenuScene(context),
-		ScenePlay:  NewPlayScene(context),
+		SceneIntro:          NewIntroScene(context),
+		SceneMenu:           NewMenuScene(context),
+		ScenePlay:           NewPlayScene(context),
+		SceneTrackSelection: NewTrackSelectionScene(context),
 	}
 	return SceneMap
 }
---
+++ internal/game/scenes/scene_track_selection.go
@@ -0,0 +1,78 @@
+package gamescene
+
+import (
+	"image"
+	"log"
+	"time"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/leandroatallah/firefly/internal/config"
+	"github.com/leandroatallah/firefly/internal/engine/assets/font"
+	"github.com/leandroatallah/firefly/internal/engine/core"
+	"github.com/leandroatallah/firefly/internal/engine/core/scene"
+	"github.com/leandroatallah/firefly/internal/engine/core/transition"
+	"github.com/leandroatallah/firefly/internal/engine/systems/audiomanager"
+)
+
+type TrackSelectionScene struct {
+	scene.BaseScene
+
+	count          int
+	audiomanager   *audiomanager.AudioManager
+	fontText       *font.FontText
+	showPressStart bool
+}
+
+func NewTrackSelectionScene(context *core.AppContext) *TrackSelectionScene {
+	fontText, err := font.NewFontText(config.Get().MainFontFace)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	scene := TrackSelectionScene{fontText: fontText}
+	scene.SetAppContext(context)
+	return &scene
+}
+
+func (s *TrackSelectionScene) OnStart() {
+	s.audiomanager = s.Manager.AudioManager()
+}
+
+func (s *TrackSelectionScene) Update() error {
+	s.count++
+
+	if ebiten.IsKeyPressed(ebiten.KeyEnter) {
+		s.Manager.NavigateTo(ScenePlay, transition.NewFader())
+	}
+
+	return nil
+}
+
+func (s *TrackSelectionScene) Draw(screen *ebiten.Image) {
+	selectionImg, _, err := ebitenutil.NewImageFromFile("assets/images/track-selection.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	frameOX, frameOY := 0, 0
+	frameSprites := 3
+	frameRate := 20
+	width := selectionImg.Bounds().Dx() / frameSprites
+	height := selectionImg.Bounds().Dy()
+
+	elementWidth := selectionImg.Bounds().Dx()
+	frameCount := elementWidth / width
+	i := (s.count / frameRate) % frameCount
+	sx, sy := frameOX+i*width, frameOY
+
+	res := selectionImg.SubImage(
+		image.Rect(sx, sy, sx+width, sy+height),
+	).(*ebiten.Image)
+
+	DrawCenteredImage(screen, res)
+}
+
+func (s *TrackSelectionScene) OnFinish() {
+	s.audiomanager.FadeOutAll(1 * time.Second)
+}
